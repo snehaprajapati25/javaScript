700c94a5de58183bcd2040797cc026848b49e31c
Sneha Prajapati <156813024+snehaprajapati25@users.noreply.github.com>
2026-02-16 09:25:22 +0000

    datatypes in js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"
+
+//alert(3+4) // we are using node.js, not browser
+
+console.log(3+4)
+
+// Data types in JavaScript
+//number => 2 to power 53
+//bigint => more than 2 to power 53
+//string => ""
+//boolean => true or false
+//null => empty value / standalone value
+//undefined => variable is declared but not assigned any value
+//symbol => unique
+
+
+//object
+
+console.log(typeof "sneha");
+
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
